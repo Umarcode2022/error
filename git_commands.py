efe1c42aa56b4c78ce0b29f0f406fcf46fab2103
batyrov2022 <batyrovpython02@gmail.com>
2022-05-08 11:36:26 +0600

    initial commit.

---
+++ git_commands.py
@@ -0,0 +1,14 @@
+git init - .git
+git remote add name url
+git pull origin master
+git status
+git add namefile
+git add .
+git add
+git commit -m "comment"
+git branch
+git branch namebranch
+git checkout namebranch
+git push namebranch
+git push origin master
+git reset filename
